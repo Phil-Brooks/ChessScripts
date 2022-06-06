1914d8c7684980ac7a28d38fab196b7e7e7e57ba
Phil-Brooks <phil@brookshome.co.uk>
2022-06-06 16:14:59 +0100

    stock

--- julia/openexp.jl
+++ julia/openexp.jl
@@ -15,4 +15,31 @@ function getmoves(ifen)
     sans
 end
 
+# looks like generates rubbish options!
+function getbestmoveblack(ifen)
+    ifen = "rnbqkbnr/pppp1ppp/4p3/8/3PP3/8/PPP2PPP/RNBQKBNR b KQkq -"
+    cfen = escapeuri(ifen)
+    addr = "https://explorer.lichess.ovh/masters?fen=" * cfen
+    resp = HTTP.get(addr)
+    str = String(resp.body)
+    jobj = JSON.Parser.parse(str)
+    moves = jobj["moves"]
+    sans = map(m -> m["san"], moves)
+    bs = map(m -> m["black"], moves)
+    ws = map(m -> m["white"], moves)
+    ds = map(m -> m["draws"], moves)
+    ts = map((w,b,d)-> w+b+d, ws,bs,ds)
+    bts = [(b,t) for (b,t) in zip(bs,ts)]
+    function f(tup) 
+        (b,t) = tup
+        b>10
+    end
+    fbts = filter(((b,t),)->b>10, bts)
+    ss = map(((b,t),)->b//t, fbts)
+    (e,i) = findmax(ss)
+
+end
+
+
+
 end
---
+++ julia/stock.jl
@@ -0,0 +1,17 @@
+module Stock
+
+using Chess
+using Chess.UCI
+
+function getbestmove(bd)
+    sf = runengine("d:\\lc0\\stockfish_15\\stockfish_15_x64_avx2.exe")
+    setoption(sf, "Threads", 12)
+    newgame(sf)
+    setboard(sf, bd)
+    ans = search(sf, "go movetime 10000", infoaction=println) #nothing)
+    bm = ans.bestmove
+    quit(sf)
+    movetosan(bd, bm)
+end
+
+end
